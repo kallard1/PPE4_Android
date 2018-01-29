21feb9fc71da4904a20fabc855e99c17520b9d91
Kevin Allard <contact@allard-kevin.fr>
2018-01-29 16:24:40 +0100

    Ajout navigation

--- app/src/main/java/fr/area42/mygavolt/MainActivity.java
+++ app/src/main/java/fr/area42/mygavolt/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.area42.mygavolt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_interventions:
+                                System.out.println("Interventions");
+                                break;
+
+                            case R.id.action_reports:
+                                System.out.println("Reports");
+                                break;
+
+                            default:
+                                System.out.println("Home");
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
     }
 }
