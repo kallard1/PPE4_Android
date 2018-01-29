71f42bfb3079c912173a9aaea6c5d581fe7c0aed
Kevin Allard <contact@allard-kevin.fr>
2018-01-29 16:37:18 +0100

    Ajout de la détection de la connectivité

--- app/src/main/java/fr/area42/mygavolt/SplashScreen.java
+++ app/src/main/java/fr/area42/mygavolt/SplashScreen.java
@@ -1,17 +1,19 @@
 package fr.area42.mygavolt;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 
 /**
- *
  * Created by allardk on 28/01/2018.
- *
  */
 
 public class SplashScreen extends Activity {
@@ -22,6 +24,10 @@ public class SplashScreen extends Activity {
         setContentView(R.layout.activity_splash);
 
         String versionName;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
 
         PackageInfo packageInfo = null;
         try {
@@ -35,14 +41,27 @@ public class SplashScreen extends Activity {
         TextView textViewVersionInfo = findViewById(R.id.textview_version_info);
         textViewVersionInfo.setText(String.format("Version: %s", versionName));
 
-        int SPLASH_TIME_OUT = 3000;
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        if (networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected()) {
+            int SPLASH_TIME_OUT = 3000;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Pas de réseau")
+                    .setMessage("Aucune connectivité détecté")
+                    .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            System.exit(0);
+                        }
+                    })
+                    .show();
+        }
     }
 }
