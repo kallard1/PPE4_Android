3c5e17ef3cd5a34ee3923976bee1f38026fef04b
Kevin Allard <contact@allard-kevin.fr>
2018-01-30 10:10:18 +0100

    Ajout du controller + vue Login

---
+++ app/src/main/java/fr/area42/mygavolt/Login.java
@@ -0,0 +1,31 @@
+package fr.area42.mygavolt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by allardk on 29/01/2018.
+ */
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        final Button loginButton = findViewById(R.id.button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
