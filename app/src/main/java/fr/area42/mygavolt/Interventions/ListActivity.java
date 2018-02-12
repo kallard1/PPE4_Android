724fa08989bb46cf5e3fc2756314b5a0607c1430
Kevin Allard <contact@allard-kevin.fr>
2018-02-12 09:43:06 +0100

    Refacto dans un helper

--- app/src/main/java/fr/area42/mygavolt/Interventions/ListActivity.java
+++ app/src/main/java/fr/area42/mygavolt/Interventions/ListActivity.java
@@ -5,27 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import fr.area42.mygavolt.API.Helper.InterventionHelper;
+import fr.area42.mygavolt.Adapter.InterventionAdapter;
 import fr.area42.mygavolt.R;
-import fr.area42.mygavolt.Utils.Adapter.InterventionAdapter;
-import fr.area42.mygavolt.Utils.Models.Intervention;
-
 
 /**
  * Created by allardk on 30/01/2018.
@@ -33,15 +20,11 @@ import fr.area42.mygavolt.Utils.Models.Intervention;
 
 public class ListActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
     RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
-
-    List<Intervention> interventionList;
-
     RequestQueue requestQueue;
 
-    String request_url = "http://api.area42.fr/interventions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,9 +46,9 @@ public class ListActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
 
-        interventionList = new ArrayList<>();
+        recyclerViewAdapter = new InterventionAdapter(ListActivity.this, InterventionHelper.sendRequest(requestQueue));
 
-        sendRequest();
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 
     @Override
@@ -80,39 +63,5 @@ public class ListActivity extends AppCompatActivity {
         onBackPressed();
         finish();
         return true;
-    }
-
-    public void sendRequest() {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, request_url, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                for (int i = 0; i < response.length(); i++) {
-
-                    Intervention intervention = new Intervention();
-
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
-
-                        intervention.setId(jsonObject.getInt("id"));
-                        intervention.setReport(jsonObject.getString("report"));
-                    } catch (JSONException je) {
-                        je.printStackTrace();
-                    }
-
-                    interventionList.add(intervention);
-                }
-
-                recyclerViewAdapter = new InterventionAdapter(ListActivity.this, interventionList);
-
-                recyclerView.setAdapter(recyclerViewAdapter);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.i("Volley error: ", String.valueOf(error));
-            }
-        });
-
-        requestQueue.add(jsonArrayRequest);
     }
 }
