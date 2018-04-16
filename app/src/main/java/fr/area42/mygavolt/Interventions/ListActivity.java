8c91f8d2f7fe76da00dce41674f350d83a579181
Kevin Allard <contact@allard-kevin.fr>
2018-04-16 15:32:28 +0200

    Filtre sur les interventions de l'employé

--- app/src/main/java/fr/area42/mygavolt/Interventions/ListActivity.java
+++ app/src/main/java/fr/area42/mygavolt/Interventions/ListActivity.java
@@ -1,6 +1,7 @@
 package fr.area42.mygavolt.Interventions;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -58,7 +63,9 @@ public class ListActivity extends AppCompatActivity {
         super.onResume();
 
         InterventionAsyncTask asyncTask = new InterventionAsyncTask();
-        asyncTask.execute("http://api.area42.fr/interventions");
+        SharedPreferences sharedPreferences = new SecurePreferences(getApplicationContext());
+
+        asyncTask.execute("http://api.area42.fr/interventions?employee=" + sharedPreferences.getInt("id", 0));
     }
 
     @Override
